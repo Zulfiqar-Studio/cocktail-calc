9a21a6092bf0c98521209a798b7425df229e2052
Zulfiqar-Studio <katrinawong.wsi@gmail.com>
2026-06-01 18:11:09 +0800

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- 這行是關鍵！強制使用相對路徑，網頁才不會走丟變白
+  base: './',
 })
